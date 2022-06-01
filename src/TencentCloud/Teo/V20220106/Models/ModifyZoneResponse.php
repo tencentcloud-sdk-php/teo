0be2a1ab20197e3d4ddb67abc53e1726c3acd48c
tencentcloudapi <tencentcloudapi@tencent.com>
2022-06-02 07:42:44 +0800

    release 3.0.641

--- src/TencentCloud/Teo/V20220106/Models/ModifyZoneResponse.php
+++ src/TencentCloud/Teo/V20220106/Models/ModifyZoneResponse.php
@@ -18,51 +18,49 @@ namespace TencentCloud\Teo\V20220106\Models;
 use TencentCloud\Common\AbstractModel;
 
 /**
- * 站点信息
+ * ModifyZone返回参数结构体
  *
- * @method string getId() 获取站点ID
- * @method void setId(string $Id) 设置站点ID
+ * @method string getId() 获取站点 ID
+ * @method void setId(string $Id) 设置站点 ID
  * @method string getName() 获取站点名称
  * @method void setName(string $Name) 设置站点名称
- * @method array getOriginalNameServers() 获取站点当前使用的 NS 列表
- * @method void setOriginalNameServers(array $OriginalNameServers) 设置站点当前使用的 NS 列表
+ * @method array getOriginalNameServers() 获取站点当前使用的 NS
+ * @method void setOriginalNameServers(array $OriginalNameServers) 设置站点当前使用的 NS
+ * @method string getStatus() 获取站点状态
+- pending 未接入 NS
+- active 已接入 NS
+- moved NS 已切走
+ * @method void setStatus(string $Status) 设置站点状态
+- pending 未接入 NS
+- active 已接入 NS
+- moved NS 已切走
+ * @method string getType() 获取站点接入方式
+- full NS 接入
+- partial CNAME 接入
+ * @method void setType(string $Type) 设置站点接入方式
+- full NS 接入
+- partial CNAME 接入
  * @method array getNameServers() 获取腾讯云分配的 NS 列表
  * @method void setNameServers(array $NameServers) 设置腾讯云分配的 NS 列表
- * @method string getStatus() 获取站点状态
-- active：NS 已切换
-- pending：NS 未切换
-- moved：NS 已切走
-- deactivated：被封禁
- * @method void setStatus(string $Status) 设置站点状态
-- active：NS 已切换
-- pending：NS 未切换
-- moved：NS 已切走
-- deactivated：被封禁
- * @method string getType() 获取站点接入方式
-- full：NS 接入
-- partial：CNAME 接入
- * @method void setType(string $Type) 设置站点接入方式
-- full：NS 接入
-- partial：CNAME 接入
- * @method boolean getPaused() 获取站点是否关闭
- * @method void setPaused(boolean $Paused) 设置站点是否关闭
- * @method string getCreatedOn() 获取站点创建时间
- * @method void setCreatedOn(string $CreatedOn) 设置站点创建时间
- * @method string getModifiedOn() 获取站点修改时间
- * @method void setModifiedOn(string $ModifiedOn) 设置站点修改时间
+ * @method string getCreatedOn() 获取创建时间
+ * @method void setCreatedOn(string $CreatedOn) 设置创建时间
+ * @method string getModifiedOn() 获取修改时间
+ * @method void setModifiedOn(string $ModifiedOn) 设置修改时间
  * @method string getCnameStatus() 获取cname 接入状态
-- finished 站点已验证
+- finished 站点验证完成
 - pending 站点验证中
 注意：此字段可能返回 null，表示取不到有效值。
  * @method void setCnameStatus(string $CnameStatus) 设置cname 接入状态
-- finished 站点已验证
+- finished 站点验证完成
 - pending 站点验证中
 注意：此字段可能返回 null，表示取不到有效值。
+ * @method string getRequestId() 获取唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
+ * @method void setRequestId(string $RequestId) 设置唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
  */
-class Zone extends AbstractModel
+class ModifyZoneResponse extends AbstractModel
 {
     /**
-     * @var string 站点ID
+     * @var string 站点 ID
      */
     public $Id;
 
@@ -72,9 +70,24 @@ class Zone extends AbstractModel
     public $Name;
 
     /**
-     * @var array 站点当前使用的 NS 列表
+     * @var array 站点当前使用的 NS
      */
     public $OriginalNameServers;
+
+    /**
+     * @var string 站点状态
+- pending 未接入 NS
+- active 已接入 NS
+- moved NS 已切走
+     */
+    public $Status;
+
+    /**
+     * @var string 站点接入方式
+- full NS 接入
+- partial CNAME 接入
+     */
+    public $Type;
 
     /**
      * @var array 腾讯云分配的 NS 列表
@@ -82,64 +95,47 @@ class Zone extends AbstractModel
     public $NameServers;
 
     /**
-     * @var string 站点状态
-- active：NS 已切换
-- pending：NS 未切换
-- moved：NS 已切走
-- deactivated：被封禁
-     */
-    public $Status;
-
-    /**
-     * @var string 站点接入方式
-- full：NS 接入
-- partial：CNAME 接入
-     */
-    public $Type;
-
-    /**
-     * @var boolean 站点是否关闭
-     */
-    public $Paused;
-
-    /**
-     * @var string 站点创建时间
+     * @var string 创建时间
      */
     public $CreatedOn;
 
     /**
-     * @var string 站点修改时间
+     * @var string 修改时间
      */
     public $ModifiedOn;
 
     /**
      * @var string cname 接入状态
-- finished 站点已验证
+- finished 站点验证完成
 - pending 站点验证中
 注意：此字段可能返回 null，表示取不到有效值。
      */
     public $CnameStatus;
 
     /**
-     * @param string $Id 站点ID
+     * @var string 唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
+     */
+    public $RequestId;
+
+    /**
+     * @param string $Id 站点 ID
      * @param string $Name 站点名称
-     * @param array $OriginalNameServers 站点当前使用的 NS 列表
-     * @param array $NameServers 腾讯云分配的 NS 列表
+     * @param array $OriginalNameServers 站点当前使用的 NS
      * @param string $Status 站点状态
-- active：NS 已切换
-- pending：NS 未切换
-- moved：NS 已切走
-- deactivated：被封禁
+- pending 未接入 NS
+- active 已接入 NS
+- moved NS 已切走
      * @param string $Type 站点接入方式
-- full：NS 接入
-- partial：CNAME 接入
-     * @param boolean $Paused 站点是否关闭
-     * @param string $CreatedOn 站点创建时间
-     * @param string $ModifiedOn 站点修改时间
+- full NS 接入
+- partial CNAME 接入
+     * @param array $NameServers 腾讯云分配的 NS 列表
+     * @param string $CreatedOn 创建时间
+     * @param string $ModifiedOn 修改时间
      * @param string $CnameStatus cname 接入状态
-- finished 站点已验证
+- finished 站点验证完成
 - pending 站点验证中
 注意：此字段可能返回 null，表示取不到有效值。
+     * @param string $RequestId 唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
     {
@@ -166,10 +162,6 @@ class Zone extends AbstractModel
             $this->OriginalNameServers = $param["OriginalNameServers"];
         }
 
-        if (array_key_exists("NameServers",$param) and $param["NameServers"] !== null) {
-            $this->NameServers = $param["NameServers"];
-        }
-
         if (array_key_exists("Status",$param) and $param["Status"] !== null) {
             $this->Status = $param["Status"];
         }
@@ -178,8 +170,8 @@ class Zone extends AbstractModel
             $this->Type = $param["Type"];
         }
 
-        if (array_key_exists("Paused",$param) and $param["Paused"] !== null) {
-            $this->Paused = $param["Paused"];
+        if (array_key_exists("NameServers",$param) and $param["NameServers"] !== null) {
+            $this->NameServers = $param["NameServers"];
         }
 
         if (array_key_exists("CreatedOn",$param) and $param["CreatedOn"] !== null) {
@@ -192,6 +184,10 @@ class Zone extends AbstractModel
 
         if (array_key_exists("CnameStatus",$param) and $param["CnameStatus"] !== null) {
             $this->CnameStatus = $param["CnameStatus"];
+        }
+
+        if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
+            $this->RequestId = $param["RequestId"];
         }
     }
 }
