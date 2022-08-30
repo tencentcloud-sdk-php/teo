96dd00e3e8b5cf5d15b5f8f9f7f1e38ee6fe1e30
tencentcloudapi <tencentcloudapi@tencent.com>
2022-08-30 16:44:15 +0800

    release 3.0.713

--- src/TencentCloud/Teo/V20220106/Models/DescribeDDoSPolicyResponse.php
+++ src/TencentCloud/Teo/V20220106/Models/DescribeDDoSPolicyResponse.php
@@ -20,17 +20,25 @@ use TencentCloud\Common\AbstractModel;
 /**
  * DescribeDDoSPolicy返回参数结构体
  *
+ * @method DdosRule getDdosRule() 获取DDoS防护配置
+ * @method void setDdosRule(DdosRule $DdosRule) 设置DDoS防护配置
  * @method string getRequestId() 获取唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
  */
 class DescribeDDoSPolicyResponse extends AbstractModel
 {
     /**
+     * @var DdosRule DDoS防护配置
+     */
+    public $DdosRule;
+
+    /**
      * @var string 唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
      */
     public $RequestId;
 
     /**
+     * @param DdosRule $DdosRule DDoS防护配置
      * @param string $RequestId 唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
@@ -46,6 +54,11 @@ class DescribeDDoSPolicyResponse extends AbstractModel
         if ($param === null) {
             return;
         }
+        if (array_key_exists("DdosRule",$param) and $param["DdosRule"] !== null) {
+            $this->DdosRule = new DdosRule();
+            $this->DdosRule->deserialize($param["DdosRule"]);
+        }
+
         if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
             $this->RequestId = $param["RequestId"];
         }
