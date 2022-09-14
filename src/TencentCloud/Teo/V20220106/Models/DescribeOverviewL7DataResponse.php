ef11b5b119e4a87961fdddbe68341b6a25546618
tencentcloudapi <tencentcloudapi@tencent.com>
2022-09-14 16:40:22 +0800

    release 3.0.725

--- src/TencentCloud/Teo/V20220106/Models/DescribeOverviewL7DataResponse.php
+++ src/TencentCloud/Teo/V20220106/Models/DescribeOverviewL7DataResponse.php
@@ -20,29 +20,29 @@ use TencentCloud\Common\AbstractModel;
 /**
  * DescribeOverviewL7Data返回参数结构体
  *
- * @method string getType() 获取查询维度
- * @method void setType(string $Type) 设置查询维度
- * @method string getInterval() 获取时间间隔
- * @method void setInterval(string $Interval) 设置时间间隔
- * @method array getData() 获取详细数据
- * @method void setData(array $Data) 设置详细数据
+ * @method string getType() 获取查询维度。
+ * @method void setType(string $Type) 设置查询维度。
+ * @method string getInterval() 获取查询时间间隔。
+ * @method void setInterval(string $Interval) 设置查询时间间隔。
+ * @method array getData() 获取七层监控类时序流量数据列表。
+ * @method void setData(array $Data) 设置七层监控类时序流量数据列表。
  * @method string getRequestId() 获取唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
  */
 class DescribeOverviewL7DataResponse extends AbstractModel
 {
     /**
-     * @var string 查询维度
+     * @var string 查询维度。
      */
     public $Type;
 
     /**
-     * @var string 时间间隔
+     * @var string 查询时间间隔。
      */
     public $Interval;
 
     /**
-     * @var array 详细数据
+     * @var array 七层监控类时序流量数据列表。
      */
     public $Data;
 
@@ -52,9 +52,9 @@ class DescribeOverviewL7DataResponse extends AbstractModel
     public $RequestId;
 
     /**
-     * @param string $Type 查询维度
-     * @param string $Interval 时间间隔
-     * @param array $Data 详细数据
+     * @param string $Type 查询维度。
+     * @param string $Interval 查询时间间隔。
+     * @param array $Data 七层监控类时序流量数据列表。
      * @param string $RequestId 唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
